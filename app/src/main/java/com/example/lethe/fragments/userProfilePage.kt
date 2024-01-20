c7162856c3bce5ca06de04dd3e5ec50f575e1298
berkecankocyigit <cagla.cengiz@ozu.edu.tr>
2024-01-20 15:06:00 +0300

    sign out fix

--- app/src/main/java/com/example/lethe/fragments/userProfilePage.kt
+++ app/src/main/java/com/example/lethe/fragments/userProfilePage.kt
@@ -6,28 +6,37 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.findNavController
 import com.example.lethe.R
+import com.example.lethe.databinding.FragmentLoginPageBinding
+import com.example.lethe.databinding.FragmentUserProfilePageBinding
 import com.example.lethe.viewmodel.MainViewModel
+import com.google.firebase.auth.FirebaseAuth
 
 class userProfilePage : Fragment() {
 
-    companion object {
-        fun newInstance() = userProfilePage()
-    }
 
     private val viewModel: MainViewModel by activityViewModels()
-
+    private lateinit var binding : FragmentUserProfilePageBinding
+    private lateinit var auth : FirebaseAuth
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_user_profile_page, container, false)
+        binding = FragmentUserProfilePageBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         //viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         // TODO: Use the ViewModel
+        auth = FirebaseAuth.getInstance()
+        binding.logoutButton.setOnClickListener {
+            auth.signOut()
+            //view?.findNavController()?.navigate(R.id.loginPage)
+            view?.findNavController()?.navigate(R.id.action_userProfilePage_to_loginPage)
+        }
     }
 
 }
